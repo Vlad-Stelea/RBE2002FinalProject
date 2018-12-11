d2e8165aa0dbedb481d4960f1a511c2e6f435072
Vlad Stelea <vladstelea@gmail.com>
2018-12-10 23:33:21 -0500

    Changed ports after rewiring

--- src/SubSystems/DriveTrain.h
+++ src/SubSystems/DriveTrain.h
@@ -8,10 +8,15 @@
 #ifndef SRC_SUBSYSTEMS_DRIVETRAIN_H_
 #define SRC_SUBSYSTEMS_DRIVETRAIN_H_
 #include "../Components/HBridgeEncoderPIDMotor.h"
+#include "../Components/FireTracker.h"
 
 #include "../Components/Gyro.h"
 class DriveTrain {
 public:
+	struct currentLoc{
+		int x, y;
+	};
+
 	DriveTrain();
 	void setup();
 	/**
@@ -21,7 +26,8 @@ public:
 	 */
 	void rotateAngle(int degrees);
 	/**
-	 * Drive a specified distance
+	 * Sets a goal to drive a specified distance
+	 * 	Keep calling loop to run it farther
 	 * @param inches the number of inches to drive
 	 */
 	void driveDistance(double inches);
@@ -40,16 +46,28 @@ public:
 	 * Called to run the drive train at the current setPoints
 	 */
 	void loop();
+
+	/**
+	 * Line up to the fire after already being found
+	 */
+	void lineUpFire();
+
+	/**
+	 * Sends the robot back to home base
+	 */
+	void goETGoHome();
+
 	virtual ~DriveTrain();
 private:
 	HBridgeEncoderPIDMotor leftMotor;
 	HBridgeEncoderPIDMotor rightMotor;
 	Gyro gyro;
+	FireTracker tracker;
 	//TODO make sure they work
 	const int leftServPin = 32;
-	const int leftEncPinA = 14;
-	const int leftEncPinB = 27;
-	const int leftDirPin = 26;
+	const int leftEncPinA = 17;
+	const int leftEncPinB = 16;
+	const int leftDirPin = 5;
 
 	const int rightServPin = 33;
 	const int rightEncPinA = 34;
@@ -58,7 +76,22 @@ private:
 
 	const int turningLeeway = 2;
 
+	/**
+	 * PID CONSTANTS
+	 */
 	const double turningKP = .1;
+
+	//Left drive motor constants
+	const double leftKP = .1;
+	const double leftKI = 0;
+	const double leftKD = 0;
+	//Right drive motor constants
+	const double rightKP = .1;
+	const double rightKI = 0;
+	const double rightKD = 0;
+
+	//keeps track of whether a command is still executing
+	bool driving;
 
 };
 
